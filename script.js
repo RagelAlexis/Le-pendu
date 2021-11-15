3060779e37af3a13a2c68387fcd8d6a8e3c13062
AlexisR <alexis.rageldu76@hotmail.fr>
2021-11-15 16:30:01 +0100

    identification d'un bouton

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const myKeyboard = document.getElementById("myKeyboard");
+const petName = document.getElementById("petName");
 const petList = [
     "crevette",
     "pingouin",
@@ -12,11 +13,20 @@ const petList = [
 let pet = petList[aleaNb(0, petList.length)];
 console.log(pet);
 
+const arrayPet = pet.split("");
+
+arrayPet.forEach((element, index) => {
+    let myIndex = index;
+    let myCase = `<div class="myLetter border border-dark col-lg-1 col-1 rounded text-center" id="indexLetter${myIndex}">_</div>`;
+    petName.innerHTML += myCase;
+});
+
+
 alphabet.forEach(element => {
-    let myLetter = element;
-    myLetter = myLetter.toUpperCase();
-    let myKey = `<button class="m-2 btn btn-outline-primary col-2" id="myKey${myLetter}">${myLetter}</button>`;
-    myKeyboard.innerHTML += (myKey);
+    let myKey = element;
+    myKey = myKey.toUpperCase();
+    let myKeyButton = `<button class="m-2 btn btn-outline-primary col-2" id="myKey${myKey}">${myKey}</button>`;
+    myKeyboard.innerHTML += myKeyButton;
 });
 
 function aleaNb(min, max) {
@@ -24,3 +34,66 @@ function aleaNb(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 };
+
+myKeyboard.addEventListener("click", (e) => {
+    if (e.target.nodeName == "BUTTON") {
+        console.log(e.target.textContent);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function compare(word, caractere) {
+//     let myCar = caractere.toLowerCase()
+//     let indexChar = []
+//     word.forEach((element, index) => {
+//         if (myCar === element) {
+//             indexChar.push(index)
+//         }
+//     })
+//     return indexChar.length == 0 ? -1 : indexChar
+// }
+
+// myPet.forEach(element => {
+//     let myLetterPet = element;
+//     myLetterPet = myLetterPet.toUpperCase();
+//     let myKeyName = `<span id="myKeyName${myLetterPet}">_ </span>`;
+//     petName.innerHTML += (myKeyName);
+// });
+
+// myKeyboard.addEventListener("click", (e) => {
+//     if (e.target.nodeName == "BUTTON") {
+//         let myLetter = e.target.id
+//         console.log(compare(myPet, myLetter));
+//     }
+
+// })
